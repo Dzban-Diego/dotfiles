19225516cbf1a2190413fcf848c84bdb12e10080
Michał Klimczewski <michal.klimczewski@gmail.com>
2025-05-28 09:23:48 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,16 @@ if status is-interactive
 end
 
 set fish_greeting
+
+# pnpm
+set -gx PNPM_HOME "/home/diego/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+
+bind ctrl-x "goto.sh"
+bind ctrl-n "edit.sh"
+
+alias n="nvim ."
